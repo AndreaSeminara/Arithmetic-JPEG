2df8a890574bf0b0f7e0cb2f3f6a66af1c8320f1
Andrea Seminara <andrea.seminara01@gmail.com>
2026-07-30 18:31:29 +0200

    feat: add main to load images + default lena image

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Arithmetic-JPEG
+
 An educational JPEG compressor comparing the standard Huffman baseline with Static Arithmetic and Adaptive Arithmetic (QM-Coder) entropy coding
+
+## Project Structure
+
+```
+arithmetic_jpeg/
+│
+├── main.py # Entry point
+```
---
+++ main.py
@@ -0,0 +1,33 @@
+# Entry Point
+
+
+import argparse
+import os
+from PIL import Image
+
+
+def main(image_path):
+    print(f"-- Inizio Pipeline Arithmetic-JPEG --")
+    print(f"Tentativo di caricamento dell'immagine : {image_path}")
+
+    if not os.path.exists(image_path):
+        print(f"Errore: Immagine non trovata in {image_path}")
+        return
+
+    img = Image.open(image_path)
+
+    altezza, larghezza = img.size
+    print(f"Dimensioni dell'immagine: {larghezza}x{altezza}")
+    print(f"Immagine caricata con successo: {image_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Pipeline Arithmetic-JPEG")
+    parser.add_argument(
+        "--image_path",
+        type=str,
+        default="images/lena.png",
+        help="Percorso dell'immagine da elaborare (default: images/lena.png)",
+    )
+    args = parser.parse_args()
+    main(image_path=args.image_path)
